088db2583108220a73d9251588f413266a5a786d
fides <fides@DESKTOP-LS7D8AJ>
2024-02-02 21:13:58 +0100

    Projektdateien hinzufügen.

---
+++ offsets.h
@@ -0,0 +1,33 @@
+#pragma once
+namespace Offsets
+{
+	constexpr auto State = 0xA51CD40;
+	constexpr auto Key = 0xA51CD78;
+	constexpr auto static_load_object = 0x4015310; // updated 
+	constexpr auto StaticFindObject = 0x4014860; // updated 
+	constexpr auto fname_pool = 0xA2E4D40;
+	constexpr auto decrypt_wide = 0x3D905B0; // updated 
+	constexpr auto decrypt_non_wide = 0x3D90540; // updated 
+	constexpr auto set_ares_outline_mode = 0x5A65670; // updated 
+	constexpr auto make_render_state_dirty = 0x5A0FED0; // updated 
+	constexpr auto ProcessEvent = 0x40001C0; // updated 
+	constexpr auto bone_matrix = 0x5AB66D0; // updated 
+	constexpr auto TriggerVEH = 0x1F2D650;
+	constexpr auto line_of_sight = 0x57B1D10; // updated 
+	constexpr auto skin_changer = 0x2E1C350; // updated 
+	constexpr auto get_firing_location = 0x3399B40;
+	constexpr auto get_spread_values_fn = 0x2DEA490;
+	constexpr auto get_spread_angles_fn = 0x35D7E20;
+	constexpr auto to_vector_and_normalize_fn = 0x3D10520; // updated 
+	constexpr auto to_angle_and_normalize_fn = 0x3D0CD10; // updated 
+	constexpr auto project_world_to_screen = 0x5BDBC20; // updated 
+	constexpr auto get_view_point = 0x5D181D0; // updated 
+	constexpr auto free_memory = 0x3C83560; // updated
+	constexpr auto ChamsOffset = 0x1190;
+}
+namespace Offsets1
+{
+    constexpr auto AcknowledgedPawn = 0x468;
+
+	
+}
